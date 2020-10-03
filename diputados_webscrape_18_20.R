ee7c5631db3ba868ef8b1a7c82afd4c84eeda892
Ben Sas Trakinsky <bsastrakinsky@wluservices-MacBook-Pro-2.local>
2020-10-03 19:50:05 -0400

    new version of scrape

---
+++ diputados_webscrape_18_20.R
@@ -0,0 +1,7 @@
+#######################################
+# Program: diputados_webscrape_18_20
+# Author: Benjamin Sas Trakinsky
+# Date: 03/10/20
+######################################
+
+# Objective: scrape results of votaciones in congress between 2018 and 2020
