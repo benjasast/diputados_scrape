b184b9874496e3ffaaa6de3851692773e841e51c
Ben Sas Trakinsky <bsastrakinsky@wluservices-MacBook-Pro-2.local>
2020-10-03 22:42:22 -0400

    function solution to mistake in regex

--- diputados_webscrape_18_20.R
+++ diputados_webscrape_18_20.R
@@ -11,20 +11,32 @@ library(magrittr)
 library(rvest)
 library(RSelenium)
 library(xml2)
+library(furrr)
 
 # Custom functions
 source('function_grab_voting_details_and_info.R')
 
 # Parameters --------------------------------------------------------------
 
-vote_id_start <- 28235
+#vote_id_start <- 28235
+
+vote_id_start <- 28930
 vote_id_end <- 36000
 
+# Create tibble of data to store ------------------------------------------
+vote_data <- tibble(vote_id = seq(vote_id_start,vote_id_end))
+
+# Vectorize function to obtain information
+grab_voting_details_and_info_V <- Vectorize(grab_voting_details_and_info)
+
+# Apply function to grab information ----------------------------------------------------------
+
+# Apply function in parallel
+plan(multisession, workers = 4)
+
+vote_info <- future_map(vote_data$vote_id,grab_voting_details_and_info, .progress = TRUE)
 
 
-
-
-
-
+grab_voting_details_and_info(28943) # Is trouble
 
 
--- function_grab_voting_details_and_info.R
+++ function_grab_voting_details_and_info.R
@@ -14,95 +14,148 @@ grab_voting_details_and_info <- function(vote_id){
   url <- str_c(vote_detalle_base_url,vote_id)
   webpage <- read_xml(url)
   
-  # Variables to scrape: voting info -----------------------------------------------------
+
+  # Check if website is empty or not ----------------------------------------
   
-  # Grab relevant content by index:
-  vote_information <- tribble(
-    ~variable, ~ index,
-    "vote_id", 1,
-    "fecha" , 2,
-    "tipo_codigo" , 3,
-    "resultado", 4,
-    "quorum", 5,
-    "sesion", 6,
-    "boletin", 7,
-    "articulo", 8,
-    "tramite", 9,
-    "informe", 10,
-    "total_afir", 11,
-    "total_neg", 12,
-    "total_abs", 13,
-    "total_dispen", 14)
+  length_website_attr <- webpage %>% 
+    xml_contents() %>% 
+    length()
   
-  # Nest variables
-  vote_information_nested <- vote_information %>% 
-    group_by(variable) %>% 
-    nest()
-  
-  # Function to grab voting info --------------------------------------------
-  grab_vote_info_variable <- function(data_df){
-    n_index <- data_df %>% 
-      select(index)
-    
-    index <- n_index[[1]]
-    
-    xml_child(webpage,index) %>% 
-      xml_contents() %>% 
-      xml_text()
+  if (length_website_attr==0){
+    print(str_c('vote_id ',vote_id, ' is invalid'))
   }
   
-  # Implement voting info function and format -------------------------------
   
-  vote_information_ready <- vote_information_nested %>% 
-    mutate(value = map(data,grab_vote_info_variable ) ) %>% 
-    unnest() %>% 
-    ungroup()
-  
-  # Format information
-  vote_id_data <- vote_information_ready %>% 
-    filter(variable=="vote_id") %>% 
-    select(value) %>% 
-    first()
-  
-  vote_information_format <- vote_information_ready %>% 
-    filter(variable!="vote_id") %>% 
-    mutate(vote_id = vote_id_data)
-  
-  
-  # Grab voting detalle -----------------------------------------------------
-  
-  vote_detalle <- tribble(
-    ~ variable, ~ index,
-    "votos", 15,
-    "pareos", 16)
-  
-  vote_detalle_nested <- vote_detalle %>% 
-    group_by(variable) %>% 
-    nest()
-  
-  vote_detalle_ready <- vote_detalle_nested %>% 
-    mutate(value = map(data,grab_vote_info_variable))
-  
-  # Parse voting information - I am not looking into pareos as of now
-  df_vote_details <- tibble(
-    diputado_id = vote_detalle_ready$value[[1]] %>% 
-      str_extract_all(("\\d+")) %>% 
-      unlist(),
-    vote = vote_detalle_ready$value[[1]] %>% 
-      str_extract_all(("AFIRMATIVO|ABSTENCION|NEGATIVO|EN CONTRA")) %>% 
-      unlist() )
-  
-  df_vote_details_format <- df_vote_details %>% 
-    mutate(vote_id = vote_id_data)
-  
-  
-  # Output with all information ---------------------------------------------
-  
-  # Output will be a list containing the voting information (metadata) and voting details
-  list_vote_output <- list(vote_information_format,df_vote_details_format)
-  
-  # Return output
-  list_vote_output
-
+  # Continue only if website is not-empty
+  if (length_website_attr>0){
+    
+    # Variables to scrape: voting info -----------------------------------------------------
+    
+    # Grab relevant content by index:
+    vote_information <- tribble(
+      ~variable, ~ index,
+      "vote_id", 1,
+      "fecha" , 2,
+      "tipo_codigo" , 3,
+      "resultado", 4,
+      "quorum", 5,
+      "sesion", 6,
+      "boletin", 7,
+      "articulo", 8,
+      "tramite", 9,
+      "informe", 10,
+      "total_afir", 11,
+      "total_neg", 12,
+      "total_abs", 13,
+      "total_dispen", 14)
+    
+    # Nest variables
+    vote_information_nested <- vote_information %>% 
+      group_by(variable) %>% 
+      nest()
+    
+    # Function to grab voting info --------------------------------------------
+    grab_vote_info_variable <- function(data_df,text_option=TRUE){
+      n_index <- data_df %>% 
+        select(index)
+      
+      index <- n_index[[1]]
+      
+      # Text option
+      if (text_option==TRUE){
+        xml_child(webpage,index) %>% 
+          xml_contents() %>% 
+          xml_text()
+      }
+      
+      else{
+        # Non-text option
+        xml_child(webpage,index) %>% 
+          xml_contents()
+      }
+      
+    }
+    
+    # Implement voting info function and format -------------------------------
+    
+    vote_information_ready <- vote_information_nested %>% 
+      mutate(value = map(data,grab_vote_info_variable ) ) %>% 
+      unnest(cols = c(data, value)) %>% 
+      ungroup()
+    
+    # Format information
+    vote_id_data <- vote_information_ready %>% 
+      filter(variable=="vote_id") %>% 
+      select(value) %>% 
+      first()
+    
+    vote_information_format <- vote_information_ready %>% 
+      filter(variable!="vote_id") %>% 
+      mutate(vote_id = vote_id_data)
+    
+    
+    # Grab voting detalle -----------------------------------------------------
+    
+    vote_detalle <- tribble(
+      ~ variable, ~ index,
+      "votos", 15,
+      "pareos", 16)
+    
+    vote_detalle_nested <- vote_detalle %>% 
+      group_by(variable) %>% 
+      nest()
+    
+    # Aux function for map()
+    grab_vote_info_variable_for_details <- function(data_df){
+      grab_vote_info_variable(data_df,text_option = FALSE)
+    }
+    
+    vote_detalle_ready <- vote_detalle_nested %>% 
+      mutate(value = map(data,grab_vote_info_variable_for_details))
+    
+    # Parse voting look for the ID of each congressman present, grab its ID
+    # and voting action.
+    
+    # Voting information
+    voting_to_parse <- vote_detalle_ready$value[[1]]
+    
+    # Parse voting information
+    tibble(list_index = 1:length(voting_to_parse))
+    
+    
+    parse_vote_detail <- function(index){
+      
+      
+    }
+    
+      diputado_id <-  xml_child(voting_to_parse,1) %>% 
+               xml_child(1) %>% 
+               xml_contents() %>% 
+               xml_text()
+    
+    xml_child(voting_to_parse,3) %>% 
+      xml_children()
+    
+    df_vote_details <- tibble(
+      diputado_id = vote_detalle_ready$value[[1]] %>% 
+        str_extract_all(("\\d+")) %>% 
+        unlist(),
+      vote = vote_detalle_ready$value[[1]] %>% 
+        str_extract_all(("AFIRMATIVO|ABSTENCION|NEGATIVO|EN CONTRA")) %>% 
+        unlist() )
+    
+    df_vote_details_format <- df_vote_details %>% 
+      mutate(vote_id = vote_id_data)
+    
+    
+    # Output with all information ---------------------------------------------
+    
+    # Output will be a list containing the voting information (metadata) and voting details
+    list_vote_output <- list(vote_information_format,df_vote_details_format)
+    
+    # Return output
+    list_vote_output
+    
+  }
 
 }
