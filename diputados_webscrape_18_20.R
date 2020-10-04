a9c9f3193b071094ee61d833bf0500c19d00b3ee
Ben Sas Trakinsky <bsastrakinsky@wluservices-MacBook-Pro-2.local>
2020-10-03 21:11:32 -0400

    function to grab votes based on vote_id

--- diputados_webscrape_18_20.R
+++ diputados_webscrape_18_20.R
@@ -5,3 +5,26 @@
 ######################################
 
 # Objective: scrape results of votaciones in congress between 2018 and 2020
+
+library(tidyverse)
+library(magrittr)
+library(rvest)
+library(RSelenium)
+library(xml2)
+
+# Custom functions
+source('function_grab_voting_details_and_info.R')
+
+# Parameters --------------------------------------------------------------
+
+vote_id_start <- 28235
+vote_id_end <- 36000
+
+
+
+
+
+
+
+
+
---
+++ function_grab_voting_details_and_info.R
@@ -0,0 +1,108 @@
+####################################################################
+# Program: function_grab_voting_details_and_info
+# Author: Benjamin Sas Trakinsky
+# Fecha: 03/10/20
+####################################################################
+
+grab_voting_details_and_info <- function(vote_id){
+
+  # Function - visit vote_id and extract votes ------------------------------
+  vote_detalle_base_url <- 'http://opendata.congreso.cl/wscamaradiputados.asmx/getVotacion_Detalle?prmVotacionID='
+  
+  # Visit website -----------------------------------------------------------
+  
+  url <- str_c(vote_detalle_base_url,vote_id)
+  webpage <- read_xml(url)
+  
+  # Variables to scrape: voting info -----------------------------------------------------
+  
+  # Grab relevant content by index:
+  vote_information <- tribble(
+    ~variable, ~ index,
+    "vote_id", 1,
+    "fecha" , 2,
+    "tipo_codigo" , 3,
+    "resultado", 4,
+    "quorum", 5,
+    "sesion", 6,
+    "boletin", 7,
+    "articulo", 8,
+    "tramite", 9,
+    "informe", 10,
+    "total_afir", 11,
+    "total_neg", 12,
+    "total_abs", 13,
+    "total_dispen", 14)
+  
+  # Nest variables
+  vote_information_nested <- vote_information %>% 
+    group_by(variable) %>% 
+    nest()
+  
+  # Function to grab voting info --------------------------------------------
+  grab_vote_info_variable <- function(data_df){
+    n_index <- data_df %>% 
+      select(index)
+    
+    index <- n_index[[1]]
+    
+    xml_child(webpage,index) %>% 
+      xml_contents() %>% 
+      xml_text()
+  }
+  
+  # Implement voting info function and format -------------------------------
+  
+  vote_information_ready <- vote_information_nested %>% 
+    mutate(value = map(data,grab_vote_info_variable ) ) %>% 
+    unnest() %>% 
+    ungroup()
+  
+  # Format information
+  vote_id_data <- vote_information_ready %>% 
+    filter(variable=="vote_id") %>% 
+    select(value) %>% 
+    first()
+  
+  vote_information_format <- vote_information_ready %>% 
+    filter(variable!="vote_id") %>% 
+    mutate(vote_id = vote_id_data)
+  
+  
+  # Grab voting detalle -----------------------------------------------------
+  
+  vote_detalle <- tribble(
+    ~ variable, ~ index,
+    "votos", 15,
+    "pareos", 16)
+  
+  vote_detalle_nested <- vote_detalle %>% 
+    group_by(variable) %>% 
+    nest()
+  
+  vote_detalle_ready <- vote_detalle_nested %>% 
+    mutate(value = map(data,grab_vote_info_variable))
+  
+  # Parse voting information - I am not looking into pareos as of now
+  df_vote_details <- tibble(
+    diputado_id = vote_detalle_ready$value[[1]] %>% 
+      str_extract_all(("\\d+")) %>% 
+      unlist(),
+    vote = vote_detalle_ready$value[[1]] %>% 
+      str_extract_all(("AFIRMATIVO|ABSTENCION|NEGATIVO|EN CONTRA")) %>% 
+      unlist() )
+  
+  df_vote_details_format <- df_vote_details %>% 
+    mutate(vote_id = vote_id_data)
+  
+  
+  # Output with all information ---------------------------------------------
+  
+  # Output will be a list containing the voting information (metadata) and voting details
+  list_vote_output <- list(vote_information_format,df_vote_details_format)
+  
+  # Return output
+  list_vote_output
+
+
+}
